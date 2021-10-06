ef073fe713ba2e47f8c0f07a2b0aa3fd16b73a47
Carlos M <carlosedm@protonmail.com>
2021-10-06 15:23:53 -0600

    Added color palette to theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        first: "#172646",
+        second: "#435898",
+        third: "#7b92f5",
+        fourth: "#bfd0ff;",
+        accent: "#000000",
+        error: "#000000",
+        
+      },
+    },
+  },
+});
