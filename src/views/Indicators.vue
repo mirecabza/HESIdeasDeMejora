e85bb4e75f104ce21dee84c02961fdf86639a8b8
Carlos M <carlosedm@protonmail.com>
2021-10-10 23:00:03 -0600

    monumental work happening here

--- src/views/Indicators.vue
+++ src/views/Indicators.vue
@@ -1,14 +1,22 @@
 <template>
-  <Header />
+  <v-app>
+    <v-container grid-list-xs>
+      <v-row justify="center">
+        <v-col>bar chart</v-col>
+        <v-col>pie chart</v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col>horizontal bar chart</v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
 
 export default {
   name: "Indicators",
   components: {
-    Header,
   },
 };
 </script>
