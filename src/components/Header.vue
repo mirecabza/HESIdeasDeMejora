7c1cadb44b9dc93cdf075baf74109e7e9bf4c4c4
Carlos M <carlosedm@protonmail.com>
2021-10-10 22:57:23 -0600

    Fixed taking up whole page instead of overlaying

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,38 @@
 <template>
-  <v-app>
-    <v-content>
-      <v-navigation-drawer v-model="drawer" absolute temporary>
-        <v-list dense>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-            :to="item.path"
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
+  <div>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="item.path"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-app-bar app class="first">
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title>HES</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn>
-          <v-icon>mdi-account-circle</v-icon>
-          Iniciar Sesión
-        </v-btn>
-      </v-app-bar>
-    </v-content>
-  </v-app>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app class="first">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>HES</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>
+        <v-icon>mdi-account-circle</v-icon>
+        Iniciar Sesión
+      </v-btn>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "prototyping",
+  name: "Header",
   data() {
     return {
       drawer: null,
