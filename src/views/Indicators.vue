5fb763a2182fc06f961dfe4a4b3cbe714bea77bb
Jose <pekoxx2@gmail.com>
2021-10-27 13:55:58 -0700

    graficas

--- src/views/Indicators.vue
+++ src/views/Indicators.vue
@@ -3,14 +3,20 @@
     <v-container grid-list-xs>
       <v-row justify="center">
         <v-col>bar chart</v-col>
-        <D3BarChart :config="chart_config" :datum="chart_data"></D3BarChart>
+        
         <v-col>pie chart</v-col>
+         <D3pieChart :config="chart_config" :datum="chart_data"></D3pieChart>
       </v-row>
       <v-row justify="center">
         <v-col>horizontal bar chart</v-col>
       </v-row>
     </v-container>
-    
+       <!-- Bar chart -->
+      <D3BarChart
+      :config="chart_config"
+      :datum="chart_data"
+     ></D3BarChart>
+      
   </div>
 </template>
 
@@ -21,6 +27,45 @@ export default {
   name: "Indicators",
   components: {
     D3BarChart,
+  },
+   data() {
+    return {
+      chart_data: [
+        //...
+        {hours: 1648, year: '2019'},
+        {hours: 2479, year: '2020'},
+        {hours: 3200, year: '2021'}
+      ],
+      chart_config: {
+        key: 'year',
+        values: ['hours']
+      },
+      count: 2010
+    }
+  },
+  methods: {
+    addData(){
+      const hours = Math.floor(Math.random() * 10000);
+      const year = (this.count++).toString();
+      this.chart_data.push({hours, year})
+    },
+    removeFirst(){
+      if(!this.chart_data.length) return;
+      this.chart_data.splice(0, 1);
+    },
+    removeLast(){
+      if(!this.chart_data.length) return;
+      this.chart_data.splice(this.chart_data.length-1, 1);
+    }
+  }
+  
+};
+
+    import { D3pieChart} from 'vue-d3-charts';
+    export default {
+  name: "Indicators",
+  components: {
+    D3pieChart,
   },data() {
     return {
       chart_data: [
@@ -43,13 +88,10 @@ export default {
       },
     }
   },
-};
-
-
+ };
 
 </script>
-     
+
 
 
 <style></style>
-
