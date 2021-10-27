4c3faa64d82afd3c2dcc32c6d2c2741ff307cea9
Jose <pekoxx2@gmail.com>
2021-10-27 13:34:20 -0700

    bar chart

--- src/views/Indicators.vue
+++ src/views/Indicators.vue
@@ -3,20 +3,53 @@
     <v-container grid-list-xs>
       <v-row justify="center">
         <v-col>bar chart</v-col>
+        <D3BarChart :config="chart_config" :datum="chart_data"></D3BarChart>
         <v-col>pie chart</v-col>
       </v-row>
       <v-row justify="center">
         <v-col>horizontal bar chart</v-col>
       </v-row>
     </v-container>
+    
   </div>
 </template>
 
+
 <script>
+import { D3BarChart } from 'vue-d3-charts';
 export default {
   name: "Indicators",
-  components: {},
+  components: {
+    D3BarChart,
+  },data() {
+    return {
+      chart_data: [
+        {vue: 20, d3: 62, category: 'lorem'},
+        {vue: 28, d3: 47, category: 'ipsum'},
+        {vue: 35, d3: 36, category: 'dolor'},
+        {vue: 60, d3: 24, category: 'sit'},
+        {vue: 65, d3: 18, category: 'amet'},
+      ],
+      chart_config: {
+        key: 'category',
+        values: ['vue', 'd3'],
+        orientation: 'horizontal',
+        color: {
+          keys: {
+            vue: '#41b882',
+            d3: '#b35899', 
+          },
+        },
+      },
+    }
+  },
 };
+
+
+
 </script>
+     
+
 
 <style></style>
+
