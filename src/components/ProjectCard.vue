d389d95878692ea19c919ca32ee5a3ed37537cf4
Carlos M <carlosedm@protonmail.com>
2021-11-05 15:20:29 -0700

    Better exposed project card data

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -3,17 +3,25 @@
     <v-col cols="auto">
       <v-row justify="center">
         <v-col cols="auto">
-          <v-icon>mdi-trophy</v-icon>
+          <v-icon
+            :color="items.place == 1 ? '#ffd700' : items.place == 2 ? '#c0c0c0' : '#cd7f32'"
+            >mdi-trophy</v-icon
+          >
         </v-col>
         <v-col cols="auto">
-          <h3>{{place}} LUGAR</h3>
+          <h3>{{ items.place }}° LUGAR</h3>
         </v-col>
       </v-row>
       <v-row>
         <v-card>
-          <v-img src="@/assets/logo.png" alt="" />
-          <v-card-title>{{ title }}</v-card-title>
-          <v-card-subtitle>{{ author }}</v-card-subtitle>
+          <v-img
+            style="margin: 1rem"
+            class="thumbnail"
+            src="@/assets/logo.png"
+            alt=""
+          />
+          <v-card-title>{{ items.title }}</v-card-title>
+          <v-card-subtitle>{{ items.author }}</v-card-subtitle>
           <v-card-actions>
             <v-btn icon @click="show = !show"
               ><v-icon>{{
@@ -24,7 +32,7 @@
           <v-expand-transition>
             <div v-show="show">
               <v-divider></v-divider>
-              <v-card-text> {{ description }} </v-card-text>
+              <v-card-text> {{ items.description }} </v-card-text>
             </div>
           </v-expand-transition>
         </v-card>
@@ -39,11 +47,13 @@ export default {
     show: false,
   }),
   props: {
-    place: Number,
-    img: String,
-    title: String,
-    author: String,
-    description: String,
+    items: {
+      place: Number,
+      img: String,
+      title: String,
+      author: String,
+      description: String,
+    },
   },
 };
 </script>
